ccfe3df8e199605029c1058d6f491938e4d24ede
Matthew Asaminew <masaminew@tractionguest.com>
2017-06-26 22:47:13 -0700

    Make text more visible, add link

--- src/views.js
+++ src/views.js
@@ -24,10 +24,10 @@ class Home extends Component {
     return (
       <div className="home-container">
         <div className="home-poster-container" onMouseEnter={this.onLinkHover} onMouseLeave={this.offLinkHover} >
-          <a className="home-poster-link" href="">
+          <a className="home-poster-link" href="https://www.eventbrite.com/e/current-a-pacific-northwest-feminist-electronic-art-symposium-tickets-35585335697" target="_blank" rel="noopener noreferrer">
             <img className="home-poster" src="https://s3.ca-central-1.amazonaws.com/current-symposium/background.png" alt="poster" />
             <div className={this.state.linkHover ? "home-poster-layover active" : "home-poster-layover"}>
-              GET TICKETS
+              <span className="home-poster-layover-text">GET TICKETS</span>
             </div>
           </a>
         </div>
